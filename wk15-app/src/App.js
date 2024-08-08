6045b4a20c2035fa17692ca1dfa1b5796700bbee
techydude9 <bob.ruzga@gmail.com>
2024-08-08 15:24:54 -0400

    Based code after create react app run

---
+++ wk15-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="border border-radius">Bob's Week 15 Coding Assignment</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
